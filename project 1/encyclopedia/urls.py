68fd4e44ad5715ecb13688788ab7fb0523ca68ff
Tony Daubitz <tony.daubitz@yahoo.com>
2024-06-06 18:20:14 +0200

    edit URLs

--- project 1/encyclopedia/urls.py
+++ project 1/encyclopedia/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("wiki/search/", views.search, name="search"),
-    path("wiki/new/", views.new, name="new"),
-    path("wiki/random/", views.random_entries, name="random"),
-    path("wiki/<str:entry>/edit/", views.edit, name="edit"),
-    path("wiki/<str:entry>/", views.entry, name="entry")
+    path("search/", views.search, name="search"),
+    path("new/", views.new, name="new"),
+    path("random/", views.random_entries, name="random"),
+    path("<str:entry>/edit/", views.edit, name="edit"),
+    path("<str:entry>/", views.entry, name="entry")
 ]
